b76b0b54292f2b5cb89575af55409fa7a11267b0
Marcos <zombiekiller199914@gmail.com>
2020-10-03 16:19:04 -0500

    Color and Props

---
+++ Assets/Scirpts/CoinCollect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 90 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name =="Player")
+        {
+            other.GetComponent<PlayerPoints>().points++;
+
+            Destroy(gameObject);
+        }
+    }
+}
