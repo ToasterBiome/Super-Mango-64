fc3e18de741e5cd59f4f19a1f0895d726f9d4a8e
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-10-29 18:22:25 -0500

    Added equipped item slot on hud
    You can also pick up the gloves and the icon will appear as equipped

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -7,16 +7,27 @@ public class HUD : MonoBehaviour
 {
     public Sprite[] HeartSprites;
     public Image HeartUI;
+    public Image GlovesIcon;
 
     public PlayerController player;
+    public PowerUpCollector collector;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        collector = GameObject.FindGameObjectWithTag("GlovesPickup").GetComponent<PowerUpCollector>();
+        GlovesIcon.enabled = false;
     }
 
     void Update()
     {
         HeartUI.sprite = HeartSprites[player.curHealth];
+
+        if (collector.hasPickedUpGloves)
+        {
+            GlovesIcon.enabled = true;
+        }
+
     }
+
 }
---
+++ Assets/Scripts/PowerUpCollector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpCollector : MonoBehaviour
+{
+
+    public bool hasPickedUpGloves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            hasPickedUpGloves = true;
+
+            Destroy(gameObject);
+        }
+    }
+}
