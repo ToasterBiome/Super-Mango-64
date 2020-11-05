a4b982a86cc00c73369b56a20271f9eb96077981
Marcos <zombiekiller199914@gmail.com>
2020-11-05 17:00:55 -0600

    DemoLevel 2.0

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public GameObject youWinText;
+    public float resetDelay;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    public void Win()
+    {
+        youWinText.SetActive (true);
+        Time.timeScale = .5f;
+        Invoke("Reset", resetDelay);
+    }
+
+    private void Reset()
+    {
+        Time.timeScale = 1.0f;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/GoldenBanana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenBanana : MonoBehaviour
+{
+   void OnTriggerEnter()
+   {
+        GameManager.instance.Win();
+
+        Destroy(gameObject);
+   }
+
+    void Update()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+    }
+}
