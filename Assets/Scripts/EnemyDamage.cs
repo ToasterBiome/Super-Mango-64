bb1cef215c271be2af12cc333305cee06ebb59b8
wbacchus <64868993+wbacchus@users.noreply.github.com>
2020-11-09 12:34:50 -0600

    adds sound scripts to most prefabs as well as audiosources on the level for ambient music.

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     private PlayerController player;
+    // add audiocomponent and audioclip array
+    private AudioSource source;
+    public AudioClip clip;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        // tell the computer which audiosource to use
+        source = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) ;
         player.Damage(1);
+        // play sound if player collides 
+       
+            Debug.Log("sound should play");
+
+            
+
+            source.PlayOneShot(clip);
+
+        
     }
+    
 }
---
+++ Assets/SoundASsets/Scripts/SoundTriggerBanana.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTriggerBanana : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip clip;
+    void Start()
+    {
+       
+        source = GetComponent<AudioSource>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Banana"))
+        {
+            
+                Debug.Log("banana trigger");
+                source.PlayOneShot(clip);
+            
+        }
+
+    }
+}
