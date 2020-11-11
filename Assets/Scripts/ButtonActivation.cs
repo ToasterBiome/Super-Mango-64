71bfab40d421f742107fe38c65dcc506c17bfb1d
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-11-11 09:19:31 -0600

    added Button that creates a bridge

---
+++ Assets/Scripts/ButtonActivation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivation : MonoBehaviour
+{
+    [SerializeField] GameObject spawnable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnable.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            spawnable.SetActive(true);
+        }
+    }
+}
