301633d4f1396aff30f222ea3f44f75b239aa4ca
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2020-11-13 10:50:00 -0600

    Objects now respawn after falling off the map.
    They use a RespawnableObject class that is used by the ObjectRespawn script attatched to the trigger underneith the map.

---
+++ Assets/Scripts/ObjectRespwner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRespwner : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            var pickup = other.gameObject.GetComponent<RespawnableObject>();
+
+            pickup.ReturnToOriginalPosition();
+        }
+    }
+
+}
---
+++ Assets/Scripts/RespawnableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnableObject : MonoBehaviour
+{
+
+    private Vector3 originalPosition; 
+    private Quaternion originalRotation;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = gameObject.transform.position;
+        originalRotation = new Quaternion(0, 0, 0, 0);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void ReturnToOriginalPosition()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        rb.Sleep();
+
+        gameObject.transform.position = originalPosition;
+        gameObject.transform.rotation = originalRotation;        
+    }
+}
