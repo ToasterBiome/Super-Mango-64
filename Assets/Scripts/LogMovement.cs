3f25e965069f2d16f4a7f02b5190443a7e0f10bc
Marcos <zombiekiller199914@gmail.com>
2021-03-02 15:36:27 -0600

    Logs and River Effect

---
+++ Assets/Scripts/LogMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMovement : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotspeed;
+    public float speed;
+    float wRadius = 1;
+
+    private void Update()
+    {
+        if(Vector3.Distance(waypoints[current].transform.position,transform.position)< wRadius)
+        {
+            current++;
+            if(current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/RiverPush.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RiverPush : MonoBehaviour
+{
+    public int PushForce = 2;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+            other.transform.Translate(-Vector3.right * Time.deltaTime * PushForce, Space.World);
+
+    }
+}
