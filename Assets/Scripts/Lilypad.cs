2f5c77186d0a261a388311f7cd08f5accb544add
mahrens1 <61665414+mahrens1@users.noreply.github.com>
2021-03-23 10:38:32 -0500

    Basic Lily pad script

---
+++ Assets/Scripts/Lilypad.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lilypad : MonoBehaviour
+{
+    private Animator padAnim;
+    private readonly int padAnimParam = Animator.StringToHash("isOnPad");
+    public bool isOnPad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOnPad = false;
+
+        padAnim = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isOnPad = true;
+            UpdateAnimation();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isOnPad = false;
+            UpdateAnimation();
+        }
+    }
+
+    public void UpdateAnimation()
+    {
+        padAnim.SetBool(padAnimParam, isOnPad);
+    }
+}
