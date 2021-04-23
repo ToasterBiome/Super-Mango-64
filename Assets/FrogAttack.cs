f42e823d2887bc088e22c53998d7733d2b2132ac
Marcos Sanchez <zombiekiller199914@gmail.com>
2021-04-23 16:06:31 -0500

    Frog&Log Update
    Frog now plays appropiate animations.We need patrol.Log now carries mango with him

--- Assets/FrogAttack.cs
+++ Assets/FrogAttack.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FrogAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    static Animator anim;
+    public Transform[] points;
+    int current;
+    public float speed;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        //current = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Waypoint
+        //if(transform.position != points[current].position)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    current = (current + 1) % points.Length;
+        //}
+
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+        if(Vector3.Distance(player.position,this.transform.position) < 10 && angle < 90)
+        {
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction),0.1f);
+
+            anim.SetBool("IsIdle", false);
+            if(direction.magnitude > .95)
+            {
+                this.transform.Translate(0, 0, 0.05f);
+                anim.SetBool("IsWalking", true);
+                anim.SetBool("IsAttacking", false);
+            }
+            else if(Vector3.Distance(player.position, this.transform.position) < 1.1 && angle < 45)
+            {
+                anim.SetBool("IsAttacking", true);
+                anim.SetBool("IsWalking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("IsIdle", true);
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsAttacking", false);
+        }
     }
 }
--- Assets/Scripts/LogMovement.cs
+++ Assets/Scripts/LogMovement.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class LogMovement : MonoBehaviour
 {
     public GameObject[] waypoints;
+    public GameObject player;
     int current = 0;
     float rotspeed;
     public float speed;
     float wRadius = 1;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            player.transform.parent = transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
     private void Update()
     {
         if(Vector3.Distance(waypoints[current].transform.position,transform.position)< wRadius)
