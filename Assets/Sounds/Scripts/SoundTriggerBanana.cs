05a2411f2b8af9e897c5c1d56fa7aaf354ea87f7
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2022-10-11 14:25:34 -0500

    URP Update
    - Converted most materials to at least render
    - Restructured some of the files

---
+++ Assets/Sounds/Scripts/SoundTriggerBanana.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTriggerBanana : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip Bananaclip;
+    public AudioClip Waterclip;
+    void Start()
+    {
+       
+        source = GetComponent<AudioSource>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Banana"))
+        {
+            
+                Debug.Log("banana trigger");
+                source.PlayOneShot(Bananaclip);
+            
+        }
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+         if (other.gameObject.CompareTag("Water"))
+        {
+
+            Debug.Log("water trigger");
+            source.PlayOneShot(Waterclip);
+
+        }
+    }
+}
---
+++ Assets/Sounds/Scripts/footstep.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+
+public class footstep : MonoBehaviour
+{
+    public AudioClip ThrowSound;
+        public AudioClip Feet_Dirt;
+        public AudioClip Feet_Grass;
+        public AudioClip jumpSound;
+    public AudioClip landSound;
+    public AudioClip pickupSound;
+    private AudioClip stepSound;
+    
+
+    
+
+        private AudioSource source;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        source = GetComponent<AudioSource>();
+        stepSound = Feet_Grass;
+        }
+
+        void OnTriggerEnter(Collider area)
+        {
+
+        if (area.gameObject.tag == "Dirt")
+      {
+            stepSound = Feet_Dirt;
+        }
+
+        if (area.gameObject.tag == "Grass")
+        {
+            stepSound = Feet_Grass;
+        }
+
+   }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            source.PlayOneShot(jumpSound);
+        }
+    }
+    public void Step()
+    {
+        
+            Debug.Log("play sound");
+            source.PlayOneShot(stepSound);
+       
+    }
+    public void Jump()
+    {
+      
+        
+    }
+    public void Land()
+    {
+
+        source.PlayOneShot(landSound);
+    }
+    public void Pickup()
+    {
+        
+            Debug.Log("play pickup");
+            source.PlayOneShot(pickupSound);
+        
+    }
+   public void Throw()
+    {
+        source.PlayOneShot(ThrowSound);
+    }
+
+}
